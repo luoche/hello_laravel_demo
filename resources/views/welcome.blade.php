5d658737029c04e5e31ace9bcaed149fe9de2752
wangyafei <luochenfen@sina.com>
2019-03-29 11:30:46 +0800

    use tool

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel duoduo</div>
+                <div class="title">Laravel duoduo.</div>
             </div>
         </div>
     </body>
